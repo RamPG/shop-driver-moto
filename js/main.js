0dc6ba31e1145f191500d85c115145ff12cedb4f
git config --global user.name <vlasovnew@yandex.ru>
2020-09-23 21:55:52 +0300

    marked banner-section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    $('.slider').slick({
+        dots: true,
+        prevArrow: '<bottom class="slider-btn slider-btnprev"><img src="img/left-arrow.svg" alt=""></bottom>',
+        nextArrow: '<bottom class="slider-btn slider-btnnext"><img src="img/right-arrow.svg" alt=""></bottom>',
+    });
 });
